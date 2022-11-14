67d0e42c98b539feda28d5facbe755930994dc7c
Serg Vlasov <stalkerloko2015@gmail.com>
2022-11-14 13:52:57 +0300

    Editing files

---
+++ task002/Program.cs
@@ -0,0 +1,57 @@
+﻿// ## Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.*
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] CreateArray(int row, int columns)
+{
+    int[,] numbers = new int[row, columns];
+    return numbers;
+}
+void FillArray(int[,] massiv)
+{
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            massiv[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+void PrintArray(int[,] massiv)
+{
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            Console.Write(massiv[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] SearchNumber(int[,] array, int num1, int num2)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) ;
+    }
+    if (num1 > array.GetLength(0) | num1 < 0 | num2 > array.GetLength(1) | num2 < 0)
+    {
+        Console.WriteLine("There is no such element");
+    }
+    Console.WriteLine($"Element = {array.GetValue(num1, num2)}");
+    return array;
+}
+
+int row = 4;
+int columns = 4;
+int[,] massiv = CreateArray(row, columns);
+FillArray(massiv);
+PrintArray(massiv);
+Console.WriteLine("Enter the coordinate number of the element:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+SearchNumber(massiv, num1, num2);
---
+++ task003/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateArray(int n, int m)
+{
+    int[,] massiv = new int[n, m];
+    return massiv;
+}
+int[,] FillArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = new Random().Next(1, 50);
+        }
+    }
+    return numbers;
+}
+void PrintArray(int[,] massiv)
+{
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            Console.Write(massiv[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ColAverage(int[,] massiv)
+{
+   
+    double average = 0;
+    int pows = 3;
+    int i = 0;
+    for (int j = 0; j < massiv.GetLength(1); j++)
+    {
+         double sum = 0;
+        for (i = 0; i < massiv.GetLength(0); i++ )
+        {
+            sum = sum + massiv[i,j];
+            average = Math.Round((sum / pows), 1);
+        }
+        Console.WriteLine($"Сумма:{sum} Среднеарифм:{average} ");
+    }
+    return massiv;
+}
+
+int pows = 3;
+int columns = 5;
+Console.Clear();
+int[,] array = CreateArray(pows, columns);
+FillArray(array);
+PrintArray(array);
+ColAverage(array);
